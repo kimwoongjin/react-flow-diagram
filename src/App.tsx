26296c6b8fed76288346c406f475d651998d9521
kimwoongjin <woong1536@gmail.com>
2025-03-25 12:01:19 +0900

    react flow 추가
     - node, css 추가
     - resizable 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,94 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useCallback, useRef } from 'react';
+import {
+  Background,
+  ReactFlow,
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  useReactFlow,
+  ReactFlowProvider,
+  type OnConnectEnd as OnConnectEndType,
+  type OnConnect,
+  type Node,
+  Controls,
+} from '@xyflow/react';
 
-function App() {
-  const [count, setCount] = useState(0)
+import '@xyflow/react/dist/style.css';
+import ResizableNode from './components/ResizableNode';
+
+const initialNodes: Node[] = [
+  {
+    id: '0',
+    type: 'input',
+    data: { label: 'Node' },
+    position: { x: 0, y: 50 },
+  },
+];
+
+let id = 1;
+const getId = () => `${id++}`;
+const nodeOrigin: [number, number] = [0.5, 0];
+
+const AddNodeOnEdgeDrop = () => {
+  const reactFlowWrapper = useRef(null);
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const { screenToFlowPosition } = useReactFlow();
+  const onConnect: OnConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+
+  const onConnectEnd: OnConnectEndType = useCallback(
+    (event, connectionState) => {
+      // when a connection is dropped on the pane it's not valid
+      if (!connectionState.isValid) {
+        // we need to remove the wrapper bounds, in order to get the correct position
+        const id = getId();
+        const { clientX, clientY } = 'changedTouches' in event ? event.changedTouches[0] : event;
+        const newNode = {
+          id,
+          type: 'ResizableNode',
+          position: screenToFlowPosition({
+            x: clientX,
+            y: clientY,
+          }),
+          data: { label: `Node ${id}` },
+          origin: [0.5, 0.0],
+        } as Node;
+
+        setNodes((nds) => nds.concat(newNode));
+        setEdges((eds: any) => eds.concat({ id, source: connectionState.fromNode?.id, target: id }));
+      }
+    },
+    [screenToFlowPosition]
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="wrapper" ref={reactFlowWrapper} style={{ height: '100vh', width: '100vw' }}>
+      <ReactFlow
+        style={{ backgroundColor: '#F7F9FB' }}
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        onConnectEnd={onConnectEnd}
+        fitView
+        fitViewOptions={{ padding: 2 }}
+        nodeOrigin={nodeOrigin}
+        nodeTypes={{ ResizableNode }}
+      >
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+};
 
-export default App
+const App = () => (
+  <ReactFlowProvider>
+    <AddNodeOnEdgeDrop />
+  </ReactFlowProvider>
+);
+
+export default App;
